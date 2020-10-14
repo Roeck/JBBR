4de34932b419c330b338c78a0f48890cac13ad12
Roeck <rons9n@gmail.com>
2020-10-14 16:19:06 -0400

    pagination done

--- src/App.js
+++ src/App.js
@@ -7,18 +7,18 @@ import JobsPagination from './JobsPagination';
 function App() {
   const [params,setParams]=useState({})
   const [page,setPage]=useState(1)
-  const {jobs,loading,error}=useFetchJobs(params,page)
+  const {jobs,loading,error,hasNextPage}=useFetchJobs(params,page)
 
   return (
     <Container className="my-4">
       <h1 className="mb-4">Jobs</h1>
-      <JobsPagination page={page} setPage={setPage} hasNextPage={true} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
       {loading&&<h1>Loading...</h1>}
       {error&&<h1> Error loading. Please retry.</h1>}
       {jobs.map(job => {
         return <Job key={job.id} job={job}></Job>
       })}
-      <JobsPagination page={page} setPage={setPage} hasNextPage={true} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
     </Container>
   )
 }
